5ed89052240f20a7e9ed7387956af04c3d5e2e21
Preludio <espinoza.rob@hotmail.com>
2014-06-04 15:43:17 -0700

    registrarCoordenada if
    Added a just in case scenario if the AT request is empty

--- registrarCoordenada.php
+++ registrarCoordenada.php
@@ -1,18 +1,19 @@
 <?php include ('functions.php');
-$latitude=$_GET['latitude'];
-$longitude=$_GET['longitude'];
-$device=$_GET['device'];
+if (!empty($_GET['latitude'])&&!empty($_GET['latitude']))
+{
+	$latitude=$_GET['latitude'];
+	$longitude=$_GET['longitude'];
+	$device=$_GET['device'];
 
-
-ejecutarSQLCommand("INSERT INTO  `coordinates` (
-`latitude` ,
-`longitude` ,
-`device`
-)
-VALUES (
-'$latitude' ,
-'$longitude' ,
-'$device')
-;");
-
+	ejecutarSQLCommand("INSERT INTO  `coordinates` (
+	`latitude` ,
+	`longitude` ,
+	`device`
+	)
+	VALUES (
+	'$latitude' ,
+	'$longitude' ,
+	'$device')
+	;");
+}
  ?>
