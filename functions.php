4faff4f0808b7baf82db2b121f83441148180e82
Preludio <espinoza.rob@hotmail.com>
2014-06-03 20:03:06 -0700

    Functions p3
    Please work part 3

--- functions.php
+++ functions.php
@@ -1,22 +1,17 @@
 <?php 
 header( 'Content-Type: text/html;charset=utf-8' );
 
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
-
-$dbhost = constant("DB_HOST"); // Host name 
-$dbport = constant("DB_PORT"); // Host port
-$dbusername = constant("DB_USER"); // Mysql username 
-$dbpassword = constant("DB_PASS"); // Mysql password 
-$db_name = constant("DB_NAME"); // Database name 
-
 function ejecutarSQLCommand($commando){
  
-  $mysqli = new mysqli($dbhost, $dbusername, $dbpassword, $db_name, $db_port);
-  
+	$mysqli = new mysqli(
+	$_ENV['OPENSHIFT_MYSQL_DB_HOST'],
+	$_ENV['OPENSHIFT_MYSQL_DB_USERNAME'],
+	$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'],
+	$_ENV['OPENSHIFT_APP_NAME'], // By default, app name == db name
+	$_ENV['OPENSHIFT_MYSQL_DB_PORT'],
+	$_ENV['OPENSHIFT_MYSQL_DB_SOCKET']
+	);  
+	
 /* check connection */
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
@@ -41,8 +36,15 @@ $mysqli->close();
 
 function getSQLResultSet($commando){
  
-  $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
-
+	$mysqli = new mysqli(
+	$_ENV['OPENSHIFT_MYSQL_DB_HOST'],
+	$_ENV['OPENSHIFT_MYSQL_DB_USERNAME'],
+	$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'],
+	$_ENV['OPENSHIFT_APP_NAME'], // By default, app name == db name
+	$_ENV['OPENSHIFT_MYSQL_DB_PORT'],
+	$_ENV['OPENSHIFT_MYSQL_DB_SOCKET']
+	);  
+	
 /* check connection */
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
