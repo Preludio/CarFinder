6db27573de6942ea4554000d3662b2d28ceed93c
Preludio <espinoza.rob@hotmail.com>
2014-06-03 19:54:48 -0700

    Functions p3
    Please work

--- functions.php
+++ functions.php
@@ -1,15 +1,21 @@
 <?php 
 header( 'Content-Type: text/html;charset=utf-8' );
 
-$db_host = $_ENV['OPENSHIFT_MYSQL_DB_HOST'];
-$db_user = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
-$db_pass = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
-$db_name = $_ENV['OPENSHIFT_APP_NAME'];
-$db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+
+$dbhost = constant("DB_HOST"); // Host name 
+$dbport = constant("DB_PORT"); // Host port
+$dbusername = constant("DB_USER"); // Mysql username 
+$dbpassword = constant("DB_PASS"); // Mysql password 
+$db_name = constant("DB_NAME"); // Database name 
 
 function ejecutarSQLCommand($commando){
  
-  $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
+  $mysqli = new mysqli($dbhost, $dbusername, $dbpassword, $db_name, $db_port);
   
 /* check connection */
 if ($mysqli->connect_errno) {
